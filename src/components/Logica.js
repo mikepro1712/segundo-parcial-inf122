0d173029602fe3672b1dcce0bb1480c893d2bd1d
mikepro1712 <119273964+mikepro1712@users.noreply.github.com>
2024-01-18 15:54:44 +0000

    examen

--- src/components/Logica.js
+++ src/components/Logica.js
@@ -3,7 +3,6 @@ import '../styles/Logica.css'
 import Boton from './Boton.js';
 function Logica(){
     return(
-        <div className="Principal">
         <div className="Flex-container">
             <div className="titulo">
                 <h1>Mis metas</h1>
@@ -11,15 +10,12 @@ function Logica(){
             <div className="Text-button1">
                 <Boton/>
             </div>
-        </div>
-        <div className="Flex-container2">
             <div>
                 <stan>Completadas</stan>
             </div>
             <div>
             <stan>Pendientes</stan>
             </div>
-        </div>
         </div>
     );
 }
